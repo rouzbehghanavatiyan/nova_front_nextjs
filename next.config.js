cdd5c107e6a7f21640235b1a96324ec2fb7d1618
rouzbeh ghanavatiyan <armangh131313@gmail.com>
2026-01-07 13:25:37 +0330

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  reactStrictMode: true,
+  
+  // تنظیم برای خواندن عکس‌ها اگر از کامپوننت <Image /> استفاده می‌کنید
   images: {
-    unoptimized: true,
+    domains: ['greatnovatools.ir'],
+    remotePatterns: [
+      {
+        protocol: 'http', // یا https در صورت داشتن SSL
+        hostname: 'greatnovatools.ir',
+        port: '',
+        pathname: '/storage/**',
+      },
+    ],
+  },
+
+  // هدایت درخواست‌های API به سمت NestJS
+  // این بخش عمدتا برای محیط لوکال مفید است، اما ساختار را حفظ می‌کند
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:4000/api/:path*', // آدرس بکند شما
+      },
+    ];
   },
 };
 
